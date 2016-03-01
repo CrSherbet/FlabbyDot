4d00368c752c52efb6d8399f8f14ffd0e62d947c
Warisara Inprom <Warisara Inprom>
2016-03-01 09:14:03 +0700

    set initial velocity

--- helloproject/src/GamePlayer.js
+++ helloproject/src/GamePlayer.js
@@ -1,8 +1,12 @@
 var GameLayer = cc.LayerColor.extend({
     init: function() {
-        this._super( new cc.Color( 25, 100, 125, 255 ) );
+        this._super( new cc.Color( 25, 180, 125, 255 ) );
         this.setPosition( new cc.Point( 0, 0 ) );
         
+        this.Player = new Player ();
+        this.Player.setPosition( new cc.Point(240,360));
+        this.addChild (this.Player);
+        this.Player.scheduleUpdate();
 	return true;
     }
 });

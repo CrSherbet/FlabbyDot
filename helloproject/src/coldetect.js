698dc84196d753c2910272b757535598559e451e
Warisara Inprom <Warisara Inprom>
2016-03-03 21:51:45 +0700

    correct collision

--- helloproject/src/coldetect.js
+++
@@ -1,6 +0,0 @@
-var checkPlayerPillarCollision = function( playerX, playerY, pillarX, pillarY ) {
-    if ( playerX > pillarX - 25 && ( playerY > pillarY + 25 || playerY < pillarY - 25 )){
-        return true;
-    }
-    else if ()
-};

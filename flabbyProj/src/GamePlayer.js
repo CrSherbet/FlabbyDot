3d3022e110fe11f9666193e89a5dd7e17696bc7e
Warisara Inprom <Warisara Inprom>
2016-03-08 10:30:18 +0700

    show bg

--- flabbyProj/src/GamePlayer.js
+++ flabbyProj/src/GamePlayer.js
@@ -1,31 +1,57 @@
 var GameLayer = cc.LayerColor.extend({
     init: function() {
-        this._super( new cc.Color( 25, 180, 125, 255 ) );
-        this.setPosition( new cc.Point( 0, 0 ) );
+        this.bg = new BG();
+        this.addChild( this.bg ,2 );
+        this.bg.setPosition( new cc.Point( screenWidth / 2, screenHeight / 2) );
+        this.bg.scheduleUpdate();
         
-          this.player = new Player();
+        this.player = new Player();
+        this.addChild( this.player, 3 );
         this.player.setPosition( new cc.Point( screenWidth / 2, screenHeight / 2 ) );
-        this.addChild( this.player, 1 );
         this.player.scheduleUpdate();
         
         this.pillarPair = null ;
-      
-        
+            
         this.addKeyboardHandlers();
         this.state = GameLayer.STATES.FRONT;
         this.scheduleUpdate();
         
 	return true;
     },
-     update: function( dt ) {
+    
+    update: function( dt ) {
         if ( this.state == GameLayer.STATES.STARTED ) {
-            if ( this.pillarPair && this.pillarPair.hit( this.player ) ) {
-               this.endGame();
-                this.state = GameLayer.STATES.DEAD;
-            }
+            this.checkHit(); 
         }
     },
-     addKeyboardHandlers: function() {
+    
+    checkHit: function (){
+        if(this.pillarPair.hit(this.player)){
+             this.endGame();
+        }
+    },
+    
+    startGame: function(){
+        this.state = GameLayer.STATES.STARTED;
+        this.createPillarPair();
+        this.player.start();
+        this.player.jump();  
+    },
+    
+    endGame: function(){
+        this.player.stop();
+        this.pillarPair.unscheduleUpdate();
+        this.state = GameLayer.STATES.DEAD;
+    },
+    
+    createPillarPair: function() {
+        this.pillarPair = new PillarPair();
+        this.pillarPair.setPosition( new cc.Point( screenWidth , screenHeight / 2  ) );
+        this.addChild( this.pillarPair,2);
+        this.pillarPair.scheduleUpdate();
+    },
+    
+    addKeyboardHandlers: function() {
         var self = this;
         cc.eventManager.addListener({
             event: cc.EventListener.KEYBOARD,
@@ -39,36 +65,17 @@ var GameLayer = cc.LayerColor.extend({
     },
  
     onKeyDown: function( keyCode, event ) {
-      if ( this.state == GameLayer.STATES.FRONT ) {
+        if ( this.state == GameLayer.STATES.FRONT ) {
             this.startGame();
-            this.state = GameLayer.STATES.STARTED;
         }
-       else if ( this.state == GameLayer.STATES.STARTED ) {
+        else if ( this.state == GameLayer.STATES.STARTED ) {
             this.player.jump();
         }
     },
  
     onKeyUp: function( keyCode, event ) {
-    },
-    
-    startGame: function(){
-        this.createPillarPair();
-        this.player.start();
-        this.player.jump(); 
-    },
-    endGame: function(){
-        this.player.stop();
-        if(this.pillarPair){
-            this.pillarPair.unscheduleUpdate();
-        }
-    },
-    createPillarPair: function() {
-        this.pillarPair = new PillarPair();
-        this.pillarPair.setPosition( new cc.Point( 480, 360 ) );
-        this.addChild( this.pillarPair );
-        this.pillarPair.scheduleUpdate();
     }
-
+    
 });
 
 GameLayer.STATES = {
@@ -76,6 +83,7 @@ GameLayer.STATES = {
     STARTED: 2,
     DEAD : 3
 };
+
 var StartScene = cc.Scene.extend({
     onEnter: function() {
         this._super();
